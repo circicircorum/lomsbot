294341f15025fa49be3dd9d7cb45093b4bfdc030
circicircorum <circicircorum@gmail.com>
2020-08-17 22:46:33 +0900

    yeet.

--- bot.py
+++ bot.py
@@ -37,9 +37,9 @@ class LOMS(discord.Client):
         elif msg_tokens[0] == '!fish':
             await message.channel.send('https://cdn.discordapp.com/emojis/651604101226037274.gif?v=1')
         elif msg_tokens[0] == '!concern':
-            await message.channel.send('https://cdn.discordapp.com/attachments/658813940268269668/744772320282935326/image0-12.jpg')
+            await message.channel.send('https://cdn.discordapp.com/attachments/658813940268269668/744914783660408904/image0-12.jpg')
         elif msg_tokens[0] == '!jail':
-            await message.channel.send('https://cdn.discordapp.com/attachments/658813940268269668/744772320043728918/go_to_horny_jail_bonk.jpg')
+            await message.channel.send('https://cdn.discordapp.com/attachments/658813940268269668/744914783320539166/go_to_horny_jail_bonk.jpg')
         elif msg_tokens[0] == '!heh':
             await message.channel.send('https://cdn.discordapp.com/attachments/658813940268269668/743890032665952286/tsubasa_chibi_bliss_cutout.png')
         elif msg_tokens[0] == '!jar':
