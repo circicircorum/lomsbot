6962b56fa2e838a5c6bcaf130baece7ca033c4bc
circicircorum <circicircorum@gmail.com>
2020-08-17 23:14:31 +0900

    moved imgs to a dict.

--- bot.py
+++ bot.py
@@ -20,6 +20,13 @@ class LOMS(discord.Client):
         channel = self.get_channel(730386849570357258)
         await channel.send(f'Logged on as {self.user}!')
 
+        self.img_dict = {'wah'      :   '<:migu:740390882435530792>',
+                         'fish'     :   'https://cdn.discordapp.com/emojis/651604101226037274.gif?v=1',
+                         'concern'  :   'https://cdn.discordapp.com/attachments/658813940268269668/744914783660408904/image0-12.jpg',
+                         'jail'     :   'https://cdn.discordapp.com/attachments/658813940268269668/744914783320539166/go_to_horny_jail_bonk.jpg',
+                         'heh'      :   'https://cdn.discordapp.com/attachments/658813940268269668/743890032665952286/tsubasa_chibi_bliss_cutout.png'}
+
+
     async def on_message(self, message):
         print(message, end='\n\n')
         print('Message from {0.author}: {0.content}'.format(message), end='\n\n')
@@ -30,23 +37,22 @@ class LOMS(discord.Client):
 
     async def process_command(self, message):
         msg_tokens = message.content.split()
+
         if msg_tokens[0] == '!test':
             await message.channel.send("Testing command...")
-        elif msg_tokens[0] == '!wah':
-            await message.channel.send('<:migu:740390882435530792>')
-        elif msg_tokens[0] == '!fish':
-            await message.channel.send('https://cdn.discordapp.com/emojis/651604101226037274.gif?v=1')
-        elif msg_tokens[0] == '!concern':
-            await message.channel.send('https://cdn.discordapp.com/attachments/658813940268269668/744914783660408904/image0-12.jpg')
-        elif msg_tokens[0] == '!jail':
-            await message.channel.send('https://cdn.discordapp.com/attachments/658813940268269668/744914783320539166/go_to_horny_jail_bonk.jpg')
-        elif msg_tokens[0] == '!heh':
-            await message.channel.send('https://cdn.discordapp.com/attachments/658813940268269668/743890032665952286/tsubasa_chibi_bliss_cutout.png')
+        elif msg_tokens[0] == '!list':
+            await message.channel.send('• List of reaction images:\n```' 
+                                        + '\n'.join(self.img_dict.keys())
+                                        + '```'
+                                        + '\nExample: \n'
+                                        + '```!fish```')
+        elif msg_tokens[0][1:] in self.img_dict.keys():
+            await message.channel.send(self.img_dict[msg_tokens[0][1:]])
         elif msg_tokens[0] == '!jar':
-            await message.channel.send('https://media.discordapp.net/attachments/491156706043232257/741047359752110144/jar_1.png \
-                                        https://media.discordapp.net/attachments/491156706043232257/741047360209289226/fubuki.gif \
-                                        https://media.discordapp.net/attachments/491156706043232257/741047360926384482/jar_2.png')
-            
+            await message.channel.send('https://media.discordapp.net/attachments/491156706043232257/741047359752110144/jar_1.png')
+            await message.channel.send('https://media.discordapp.net/attachments/491156706043232257/741047360209289226/fubuki.gif')
+            await message.channel.send('https://media.discordapp.net/attachments/491156706043232257/741047360926384482/jar_2.png')
+
 
 # define main function
 def main():
