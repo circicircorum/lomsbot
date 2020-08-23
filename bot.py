4985c39e833ce8fa054c91a08a4029a076e5347a
circicircorum <circicircorum@gmail.com>
2020-08-24 00:57:31 +0900

    added flags.

--- bot.py
+++ bot.py
@@ -46,7 +46,9 @@ class LOMS(discord.Client):
                         'fishbasa'  :   'https://cdn.discordapp.com/attachments/658813940268269668/747118363179876483/1584101383442.png',
                         'gun'       :   'https://cdn.discordapp.com/attachments/658813940268269668/747118452623540394/EScakDrXYAALfdb.png',
                         'derpcat'   :   'https://cdn.discordapp.com/attachments/658813940268269668/747118615643422800/mzznExn.mp4',
-                        'turkey'    :   'https://cdn.discordapp.com/attachments/658813940268269668/741303257179226252/1920px-Flag_of_Germany.png'}
+                        'turkey'    :   'https://cdn.discordapp.com/attachments/658813940268269668/741303257179226252/1920px-Flag_of_Germany.png',
+                        'france'    :   'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Flag_of_Senegal.svg/1280px-Flag_of_Senegal.svg.png'
+                        'senegal'   :   'https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/1280px-Flag_of_France.svg.png'}
 
         self.command_dict = {
             'test'          :   Command('Test Command'),
