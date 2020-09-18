5889f29380948e54c6d2675225ec2d69ca750fe8
circicircorum <circicircorum@gmail.com>
2020-09-18 09:25:24 +0900

    added random selection of img.

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
-#import psycopg2
 import os
+import random
+#import psycopg2
 import json
 from dotenv import load_dotenv
 
@@ -26,17 +27,6 @@ class LOMS(discord.Client):
         channel = self.get_channel(730386849570357258)
         await channel.send(f'Logged on as {self.user}!')
 
-        self.command_dict = {
-            'test'          :   Command('Test Command'),
-            'invite'        :   SendMessageCommand('Send Invite', message_text='Click the link below to invite LOMSBot:\nhttps://discord.com/oauth2/authorize?client_id=730301823512215563&scope=bot&permissions=1'),
-            #'fish'      :   SendMessageCommand('Fish',          message_text='https://cdn.discordapp.com/emojis/651604101226037274.gif?v=1',
-            #                                                    description='Fish.'),
-            'list-images'   :   SendFormattedMessageCommand('List Images', message_type=constants.LIST_IMAGES_COMMAND, dictionary=self.img_dict),
-            'list-info'     :   SendFormattedMessageCommand('List Inforgraphics', message_type=constants.LIST_DEFAULT_COMMAND, dictionary=self.info_dict)
-        }
-        self.command_dict['commands'] = SendFormattedMessageCommand('List Commands', message_type=constants.LIST_COMMANDS_COMMAND, dictionary=self.command_dict)
-    
-
     async def on_message(self, message):
         print(message, end='\n\n')
         print('Message from {0.author}: {0.content}'.format(message), end='\n\n')
@@ -48,12 +38,22 @@ class LOMS(discord.Client):
     async def process_command(self, message):
         msg_tokens = message.content.split()
 
-        if msg_tokens[0][1:] in self.command_dict.keys():
-            await self.command_dict[msg_tokens[0][1:]].action(discord_message=message)
-        elif msg_tokens[0][1:] in self.img_dict.keys():
-            await message.channel.send(self.img_dict[msg_tokens[0][1:]])
-        elif msg_tokens[0][1:] in self.info_dict.keys():
-            await message.channel.send(self.info_dict[msg_tokens[0][1:]])
+        # use if the number of dictionaries grow...
+        for dictionary in self.dict_list:
+            if msg_tokens[0][1:] in dictionary.keys():
+                if isinstance(dictionary[msg_tokens[0][1:]], Command):
+                    await self.command_dict[msg_tokens[0][1:]].action(discord_message=message)
+                elif isinstance(dictionary[msg_tokens[0][1:]], str):
+                    await message.channel.send(dictionary[msg_tokens[0][1:]])
+                elif isinstance(dictionary[msg_tokens[0][1:]], list):
+                    await message.channel.send(random.choice(dictionary[msg_tokens[0][1:]]))
+
+        # if msg_tokens[0][1:] in self.command_dict.keys():
+        #     await self.command_dict[msg_tokens[0][1:]].action(discord_message=message)
+        # elif msg_tokens[0][1:] in self.img_dict.keys():
+        #     await message.channel.send(random.choice(self.img_dict[msg_tokens[0][1:]]))
+        # elif msg_tokens[0][1:] in self.info_dict.keys():
+        #     await message.channel.send(random.choice(self.info_dict[msg_tokens[0][1:]]))
     
 
     def init_dict(self, filename):
@@ -61,18 +61,27 @@ class LOMS(discord.Client):
         with open('img_dict.json', 'r') as f:
             self.img_dict = json.load(f)
         
-        for key, value in self.img_dict.items():
-            if isinstance(value, list):
-                self.img_dict[key] = ' '.join(value)
+        # concatenation behaviour (old)
+        #for key, value in self.img_dict.items():
+        #    if isinstance(value, list):
+        #        self.img_dict[key] = ' '.join(value)
         
         # load info_dict
         with open('info_dict.json', 'r') as f:
             self.info_dict = json.load(f)
-        
-        for key, value in self.info_dict.items():
-            if isinstance(value, list):
-                self.info_dict[key] = ' '.join(value)
 
+        # define command_dict
+        self.command_dict = {
+            'test'          :   Command('Test Command'),
+            'invite'        :   SendMessageCommand('Send Invite', message_text='Click the link below to invite LOMSBot:\nhttps://discord.com/oauth2/authorize?client_id=730301823512215563&scope=bot&permissions=1'),
+            #'fish'      :   SendMessageCommand('Fish',          message_text='https://cdn.discordapp.com/emojis/651604101226037274.gif?v=1',
+            #                                                    description='Fish.'),
+            'list-images'   :   SendFormattedMessageCommand('List Images', message_type=constants.LIST_IMAGES_COMMAND, dictionary=self.img_dict),
+            'list-info'     :   SendFormattedMessageCommand('List Inforgraphics', message_type=constants.LIST_DEFAULT_COMMAND, dictionary=self.info_dict)
+        }
+        self.command_dict['commands'] = SendFormattedMessageCommand('List Commands', message_type=constants.LIST_COMMANDS_COMMAND, dictionary=self.command_dict)
+
+        self.dict_list = [self.command_dict, self.img_dict, self.info_dict]
 
 # define main function
 def main():
